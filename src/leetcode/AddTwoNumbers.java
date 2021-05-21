2762fa8361cda626bd10a57acf6b655be5a19c61
ksooss <ksooss51@naver.com>
2021-05-21 18:39:37 +0900

    수정

--- src/leetcode/AddTwoNumbers.java
+++ src/leetcode/AddTwoNumbers.java
@@ -14,7 +14,7 @@ public class AddTwoNumbers {
 		AddTwoNumbers obj = new AddTwoNumbers();
 		ListNode result = obj.addTwoNumbers(l1, l2);
 
-		while (result.next != null) {
+		while (result!= null) {
 			System.out.println("result : " + result.val);
 			result = result.next;
 		}
@@ -22,12 +22,34 @@ public class AddTwoNumbers {
 
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		ListNode result = new ListNode(0);
+		ListNode l3=result;
+        while(l1!=null || l2!=null) {
+            int l1Val = l1==null ? 0:l1.val;
+            int l2Val = l2==null ? 0:l2.val;
+            int l3Val = l3.val;
+            
+            int sum = l1Val + l2Val + l3Val;
+            if(sum>=10) {
+                sum -= 10;
+                l3.next = new ListNode(1);
+            }
+            l3.val = sum;
+            l1 = l1==null ? null:l1.next;
+            l2 = l2==null ? null:l2.next;
+            if((l1!=null||l2!=null)&&l3.next==null)
+            	l3.next = new ListNode(0);
+            l3 = l3.next;
+        }
+        return result;
+	}
+	
+	public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+		ListNode result = new ListNode(0);
 		sumTwoNumbers(l1, l2, result);
 		return result;
 	}
 
 	public static void sumTwoNumbers(ListNode l1, ListNode l2, ListNode result) {
-
 		int sum = l1.val + l2.val + result.val;
 		if (sum >= 10) {
 			result.next = new ListNode(1);
