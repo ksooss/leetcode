a8787e7e4dc4b59ffb3d49fc714ea33759a1c90b
ksooss <ksooss51@naver.com>
2021-05-25 12:12:30 +0900

    package 변경

--- src/problem/IsMatch.java
+++ src/problem/IsMatch.java
@@ -1,4 +1,4 @@
-package leetcode;
+package problem;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
--- src/problem/LongestPalindrome.java
+++ src/problem/LongestPalindrome.java
@@ -1,4 +1,4 @@
-package leetcode;
+package problem;
 
 public class LongestPalindrome {
 
